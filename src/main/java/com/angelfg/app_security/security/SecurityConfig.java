de4eabca8b3c4013e1bfd71c74d428e9afbbbb6a
Luis Angel <angelfgdeveloper@gmail.com>
2024-07-30 02:57:16 -0600

    Generando un usuario en memoria sin password a encriptar temporal

--- src/main/java/com/angelfg/app_security/security/SecurityConfig.java
+++ src/main/java/com/angelfg/app_security/security/SecurityConfig.java
@@ -4,6 +4,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -48,6 +53,32 @@ public class SecurityConfig {
         .httpBasic(Customizer.withDefaults());
 
         return http.build();
+    }
+
+    // 4 - Usuarios en memoria
+    @Bean
+    public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
+
+        UserDetails admin = User
+                .withUsername("admin")
+                .password("to_be_encoded") // password debe ser codificado
+                .authorities("ADMIN")
+                .build();
+
+        UserDetails user = User
+                .withUsername("user")
+                .password("to_be_encoded") // password debe ser codificado
+                .authorities("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(admin, user);
+    }
+
+    // Para pruebas de spring security para no codificar la contraseña
+    // No se debe de realizar
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
     }
 
 }
