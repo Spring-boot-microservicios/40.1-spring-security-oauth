ee9c578a97775d88ed1ca6eb34201efef32cad92
Luis Angel <angelfgdeveloper@gmail.com>
2024-07-30 18:11:02 -0600

    Uso de AuthenticationProvider para realizar el login

---
+++ src/main/java/com/angelfg/app_security/components/MyAuthenticationProvider.java
@@ -0,0 +1,50 @@
+package com.angelfg.app_security.components;
+
+import com.angelfg.app_security.entities.CustomerEntity;
+import com.angelfg.app_security.repositories.CustomerRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import java.util.Collections;
+import java.util.List;
+
+@Component
+@AllArgsConstructor
+public class MyAuthenticationProvider implements AuthenticationProvider {
+
+    private final CustomerRepository customerRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    // No es muy comun utilizar el Authentication providers, usar mejor el UserDetails => CustomerUserDetailsServiceImpl.class
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        final String username = authentication.getName();
+        final String password = authentication.getCredentials().toString(); // puede ser un token, en este caso es un string
+
+        CustomerEntity customerFromDB = this.customerRepository.findByEmail(username)
+                .orElseThrow(() -> new BadCredentialsException("Invalid Credentials"));
+
+        final String passwordFromDB = customerFromDB.getPassword();
+
+        if (passwordEncoder.matches(password, passwordFromDB)) {
+            List<SimpleGrantedAuthority> authorities = Collections.singletonList(new SimpleGrantedAuthority(customerFromDB.getRole()));
+            return new UsernamePasswordAuthenticationToken(username, password, authorities);
+        } else {
+            throw new BadCredentialsException("Invalid credentials - Password");
+        }
+    }
+
+    // No lo utilizamos pero decimos cual autenticacion soportara
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return (UsernamePasswordAuthenticationToken.class.isAssignableFrom(authentication));
+    }
+
+}
--- src/main/java/com/angelfg/app_security/security/SecurityConfig.java
+++ src/main/java/com/angelfg/app_security/security/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -48,6 +49,10 @@ public class SecurityConfig {
         )
         .formLogin(Customizer.withDefaults())
         .httpBasic(Customizer.withDefaults());
+
+        // Deshabilitar para no tener problemas en el navegador
+//        http.cors(AbstractHttpConfigurer::disable);
+//        http.csrf(AbstractHttpConfigurer::disable);
 
         return http.build();
     }
